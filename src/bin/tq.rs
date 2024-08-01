4b190c88082c40c461f4819a72f44c3d27a5da90
Venus Xeon-Blonde <venusflameblonde@gmail.com>
2024-08-01 15:26:57 -0400

    Push syntax highlighting

--- src/bin/tq.rs
+++ src/bin/tq.rs
@@ -1,33 +1,84 @@
-use clap::Parser;
-use std::process::ExitCode;
-use tq::OutputType;
+use clap::{ColorChoice, Parser};
+use toml::Value;
+use std::{fs::File, io::{self, Cursor, Read}, path::PathBuf};
 
-fn main() -> ExitCode {
-    let app = tq::Cli::parse();
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// The TOML File to read, or STDIN if none is supplied.
+    #[arg(short, long)]
+    pub file: Option<PathBuf>,
 
-    let toml_file = tq::load_toml_from_file(&app.file);
+    /// The output type. Default is TOML, but supports outputting in different formats.
+    #[arg(short, long, default_value = "toml")]
+    pub output: OutputType,
 
-    let Ok(toml_file) = toml_file else {
-        let e = toml_file.unwrap_err();
-        eprintln!("{}", e);
-        return ExitCode::FAILURE;
+    /// Field to read from the TOML file
+    pub pattern: String,
+
+    #[arg(short, long, default_value = "auto")]
+    pub color: ColorChoice
+}
+
+#[derive(Default, Debug, Clone, clap::ValueEnum)]
+pub enum OutputType {
+    #[default]
+    Toml,
+    #[cfg(feature = "json")]
+    Json,
+}
+
+fn main() -> anyhow::Result<()> {
+    let app = Cli::parse();
+
+    match app.color {
+        // console will by default respect certain environment variables for color output, 
+        // in addition to checking if the standard output is a TTY.
+        ColorChoice::Auto => {},
+        ColorChoice::Never => console::set_colors_enabled(false),
+        ColorChoice::Always => console::set_colors_enabled(true),
+    }
+
+    // Get a reader over the input to tq.
+    let mut reader: Box<dyn Read> = match &app.file {
+        Some(path) => Box::new(File::open(path)?),
+        None => Box::new(io::stdin()),
     };
 
-    let x = tq::extract_pattern(&toml_file, &app.pattern);
+    let mut input_string = String::new();
+    reader.read_to_string(&mut input_string)?;
 
-    match x {
-        Ok(needle) => {
-            match app.output {
-                OutputType::Toml => println!("{}", format!("{}", needle).trim_matches('"')),
-                #[cfg(feature = "json")]
-                OutputType::Json => println!("{}", serde_json::to_string(&needle).unwrap()),
-            }
+    let toml_value: toml::Value = toml::from_str(&input_string)?;
 
-            ExitCode::SUCCESS
-        }
-        Err(e) => {
-            eprintln!("{}", e);
-            ExitCode::FAILURE
+    let result: &Value = tq::extract_pattern(&toml_value, &app.pattern)?;
+
+    // Use `bat`'s pretty printing system to print.
+    let mut pretty_printer = bat::PrettyPrinter::new();
+    pretty_printer
+        .colored_output(console::colors_enabled())
+        .grid(false)
+        .rule(false)
+        .line_numbers(false);
+
+    match app.output {
+        OutputType::Toml => {
+            let toml_str = toml::to_string_pretty(result)?;
+            
+            pretty_printer
+                .language("toml")
+                .input_from_reader(Cursor::new(toml_str))
+                .print()?;
+        },
+
+
+        #[cfg(feature = "json")]
+        OutputType::Json => {
+            pretty_printer
+                .language("json")
+                .input_from_reader(Cursor::new(serde_json::to_string_pretty(result)?))
+                .print()?;
         }
     }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-use std::fs::File;
-use std::io::prelude::*;
-
-use clap::Parser;
 use thiserror::Error;
 use toml::Value;
 
-type Result<T> = std::result::Result<T, TqError>;
+type TqResult<T> = std::result::Result<T, TqError>;
 
 #[derive(Error, Debug)]
 pub enum TqError {
@@ -19,30 +15,7 @@ pub enum TqError {
     PatternNotFoundError { pattern: String },
 }
 
-#[derive(Default, Debug, Clone, clap::ValueEnum)]
-pub enum OutputType {
-    #[default]
-    Toml,
-    #[cfg(feature = "json")]
-    Json,
-}
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Cli {
-    /// The TOML File to read
-    #[arg(short, long, value_name = "TOML_FILE")]
-    pub file: String,
-
-    /// Field to read from the TOML file
-    pub pattern: String,
-
-    /// The output type. Default is TOML, but supports outputting in different formats.
-    #[arg(short, long, value_name = "OUTPUT_TYPE", default_value = "toml")]
-    pub output: OutputType,
-}
-
-pub fn extract_pattern<'a>(toml_file: &'a Value, pattern: &str) -> Result<&'a Value> {
+pub fn extract_pattern<'a>(toml_file: &'a Value, pattern: &str) -> TqResult<&'a Value> {
     if pattern.is_empty() || pattern == "." {
         return Ok(toml_file);
     }
@@ -58,19 +31,6 @@ pub fn extract_pattern<'a>(toml_file: &'a Value, pattern: &str) -> Result<&'a Va
         .ok_or_else(|| TqError::PatternNotFoundError {
             pattern: pattern.to_string(),
         })
-}
-
-pub fn load_toml_from_file(file_name: &str) -> Result<toml::Value> {
-    let mut file = File::open(file_name).map_err(|e| TqError::FileOpenError {
-        file_name: file_name.to_string(),
-        cause: e.to_string(),
-    })?;
-    let mut contents = String::new();
-    let _ = file.read_to_string(&mut contents);
-    toml::from_str::<Value>(&contents).map_err(|e| TqError::TomlParseError {
-        file_name: file_name.to_string(),
-        cause: e.to_string(),
-    })
 }
 
 #[cfg(test)]
